6d9312d91e5b47c2f770f78968b760efbb1cd8e9
Shunsuke Hiratsuka <hiratsukashu@indival.co.jp>
2018-01-29 22:18:18 +0900

    データ作成続き 形態素解析までオワタ

--- make_crf_data.py
+++ make_crf_data.py
@@ -18,4 +18,26 @@ def makePreCrfData(mecab,sentence):
         mergeList = nodeParts+aryFeature
         arySentence.append(mergeList)
 
-    print(arySentence)    
+    return arySentence
+
+
+# 形態素解析して、それをファイルに書き込み
+with open('all_sentence.txt','r') as f:
+
+    aryAllSentenceFeature = []
+
+    lines = f.readlines()
+
+    for line in lines:
+        if line != None:
+            arySentenceFeature = makePreCrfData(mecab,line)
+            aryAllSentenceFeature = aryAllSentenceFeature+arySentenceFeature
+    print(aryAllSentenceFeature)
+
+    with open('all_sentence_feature.txt','w') as f:
+        aryFeature = []
+        for arySentenceFeature in aryAllSentenceFeature:
+            strLine = "\t".join(arySentenceFeature)
+            aryFeature.append(strLine)
+        
+        fw.write('\n'.join(aryFeature))
